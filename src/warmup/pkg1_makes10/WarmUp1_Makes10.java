68d1e7df6d7e5ba1b509e55cd5165dbbde66d9a4
Chris Papadopoulos <xristos8170@gmail.com>
2020-01-13 14:31:01 +0200

    Add files via upload

---
+++ src/warmup/pkg1_makes10/WarmUp1_Makes10.java
@@ -0,0 +1,19 @@
+package warmup.pkg1_makes10;
+
+public class WarmUp1_Makes10 {
+
+    public static void main(String[] args) {
+        System.out.println(makes10(9, 10));
+        System.out.println(makes10(9, 9));
+        System.out.println(makes10(1, 9));
+    }
+    public static boolean makes10(int a, int b){
+        if((a == 10) || (b == 10) || (a + b == 10)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+}
